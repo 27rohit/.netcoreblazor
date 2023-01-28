976002bb1b88a18712a4f949147d5f8c3aa8217b
DESKTOP-VCHCRRG\Rohit <rohit@codeprecisesolutions.com>
2023-01-28 14:48:46 +0530

    initail commit

---
+++ Crud/Client/Services/DoctorServices/IDoctorService.cs
@@ -0,0 +1,17 @@
+﻿using Crud.Shared;
+
+namespace Crud.Client.Services.DoctorServices
+{
+    public interface IDoctorService
+    {
+        List<Doctor> Doctors { get; set; }
+        List<Ward> Wards { get; set; }
+        Task GetWards();
+        Task GetDoctors();
+        Task<Doctor> GetSingleDoctor(int Id);
+        Task CreateDoctor(Doctor doctor);
+        Task UpdateDoctor(Doctor doctor);
+        Task DeleteDoctor(int Id);
+
+    }
+}
